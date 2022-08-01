e9e5adb76bf3d1f7dbf21b5e7ff31c54e5c87c80
basilnct <57843515+basilnct@users.noreply.github.com>
2022-08-01 15:25:35 +0200

    Create nn.tcl

---
+++ vivado/nn.tcl
@@ -0,0 +1,2 @@
+set_property  ip_repo_paths  {/home/bang/Documents/hls4ml_admos_live_test/vivado/PYNQ/boards/ip /home/bang/Documents/hls4ml_admos_live_test/vivado/myproject_prj/solution1/impl} [current_project]
+update_ip_catalog
