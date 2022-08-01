0aa183eec14f60fa511486f3f28bfd1744494571
basilnct <57843515+basilnct@users.noreply.github.com>
2022-08-01 15:39:35 +0200

    Update nn.tcl

--- audio_nn/nn.tcl
+++ audio_nn/nn.tcl
@@ -1,2 +1,3 @@
-set_property  ip_repo_paths  {/home/bang/Documents/hls4ml_admos_live_test/vivado/PYNQ/boards/ip /home/bang/Documents/hls4ml_admos_live_test/vivado/myproject_prj/solution1/impl} [current_project]
+set_property  ip_repo_paths  {../../ip ../../../../myproject_prj/solution1/impl} [current_project]
 update_ip_catalog
+
