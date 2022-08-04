5fcbef500862d9726133f88e98e173eb00b3f019
basilnct <57843515+basilnct@users.noreply.github.com>
2022-08-04 16:32:08 +0200

    Add files via upload

--- training/common.py
+++ training/common.py
@@ -107,17 +107,17 @@ def file_load(wav_name, mono=False):
     """
     
     try:
-     with wave.open(wav_path, 'r') as wav_file:
-      raw_frames = wav_file.readframes(-1)
-      num_frames = wav_file.getnframes()
-      num_channels = wav_file.getnchannels()
-      sample_rate = wav_file.getframerate()
-      sample_width = wav_file.getsampwidth()
-    temp_buffer = np.empty((num_frames, num_channels, 4), dtype=np.uint8)
-    raw_bytes = np.frombuffer(raw_frames, dtype=np.uint8)
-    temp_buffer[:, :, :sample_width] = raw_bytes.reshape(-1, num_channels, sample_width)
-    temp_buffer[:, :, sample_width:] = (temp_buffer[:, :, sample_width-1:sample_width] >> 7) * 255
-    return temp_buffer.view('<i4').reshape(temp_buffer.shape[:-1])
+    	with wave.open(wav_path, 'r') as wav_file:
+    		raw_frames = wav_file.readframes(-1)
+    		num_frames = wav_file.getnframes()
+    		num_channels = wav_file.getnchannels()
+    		sample_rate = wav_file.getframerate()
+    		sample_width = wav_file.getsampwidth()
+    	temp_buffer = np.empty((num_frames, num_channels, 4), dtype=np.uint8)
+    	raw_bytes = np.frombuffer(raw_frames, dtype=np.uint8)
+    	temp_buffer[:, :, :sample_width] = raw_bytes.reshape(-1, num_channels, sample_width)
+    	temp_buffer[:, :, sample_width:] = (temp_buffer[:, :, sample_width-1:sample_width] >> 7) * 255
+    	return temp_buffer.view('<i4').reshape(temp_buffer.shape[:-1])
    
     except:
         logger.error("file_broken or not exists!! : {}".format(wav_name))
